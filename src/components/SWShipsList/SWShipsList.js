3e65a394d4a882a1ade030b69f4954972f49e84b
Elena <turonchek09@gmail.com>
2022-01-28 02:05:30 +0200

    fetch pagination added

---
+++ src/components/SWShipsList/SWShipsList.js
@@ -0,0 +1,41 @@
+import React, {Component} from "react";
+import { SWShipsItem } from "../SWShipsItem/SWShipsItem";
+import PropTypes from 'prop-types';
+
+export class SWShipsList extends Component {
+    render(){
+        const {data,status, error}=this.props;
+        return <div>
+            {status === `loading` || status === `initial` ? (
+                    <div>Loading...</div>
+                ) : (
+                    <div className="">
+                        {error === null ? (
+
+                            <ul className="card-body">
+                                {data.map((item) => (
+                                    <SWShipsItem 
+                                        key={item.uid} 
+                                        {...item}/>
+                                ))}
+                            </ul>
+                        ) : (
+                            <span style={{color:`red`}}>{error}</span>
+                        )}
+                    </div>
+                )}
+        </div>
+    }
+}
+
+SWShipsList.propTypes ={
+    data:PropTypes.arrayOf(
+        PropTypes.shape({
+            name:PropTypes.string,
+            uid:PropTypes.string,
+            url:PropTypes.string,
+        })
+    ),
+    status:PropTypes.string.isRequired,
+    error:PropTypes.string,
+}
---
+++ src/components/SWShipsPage/SWShipsPage.js
@@ -0,0 +1,76 @@
+import React, {Component} from "react";
+import { Pagination } from "../Pagination/Pagination";
+import { SWShipsList } from "../SWShipsList/SWShipsList";
+
+export class SWShipsPage extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            status: 'initial',
+            error:null,
+            data:[],
+            currentPage:1
+        };
+    }
+
+
+    handlePageClick = (data) => {
+        let currentPage=1+data.selected;
+        this.setState ({
+            currentPage,
+        }, ()=>{
+            this.fetchData();
+        })
+    }
+
+    fetchData = () => {
+        this.setState({
+            status:'loading',
+            error:null,
+            data:[],
+        });
+
+        fetch(`https://www.swapi.tech/api/starships?page=${this.state.currentPage}&limit=10`)
+            .then((res) => {
+                return res.json()
+            })
+            .then((result) => {
+                this.setState({
+                    status: `success`,
+                    error: null,
+                    data:result,
+                });
+            })
+            .catch((error) => {
+                this.setState({
+                    status: `error`,
+                    error:error.message,
+                });
+            })
+    }
+
+    render(){
+        const { status, error, data, currentPage } = this.state;
+        return(
+            <div>
+                <SWShipsList 
+                    data={data.results}
+                    status={status}
+                    error={error}
+                    />
+                {status===`success` && <Pagination 
+                                            handlePageClick={this.handlePageClick}
+                                            pageCount={data.total_pages}
+                                            currentPage={currentPage}
+                                        />
+                }
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        this.fetchData();
+    }
+}
